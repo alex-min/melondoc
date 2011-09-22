a09e8d099f51395b4fccd3561120be9880adb44b
minette.alexandre <minette.alexandre@47a1526c-3bc8-30c1-57df-79ecd9ce54fb>
2011-09-22 08:11:00 +0000

    classe d'abstraction de bdd

--- trunk/library/db.php
+++ trunk/library/db.php
@@ -5,33 +5,44 @@ class		db
   private		$_serveur;
   private		$_user;
   private		$_pass;
-  private		$_db;
-  private static	$_instances = array();
+  private    static	$_db = false;
 
   // ici on fait un multi ton
-  public function __construct()
-  {
+  public function __construct() {
   }
 
-  private function connect()
+  private function connect($serveur, $user, $pass, $bd)
   {
-
+    $this->_db = mysql_connect($serveur, $user, $pass);    
+    if (!$this->_db) {
+      errorMVC::errorSQL("Echec de connection a la base sql");
+    }
+    if (!mysql_select_db($bd)) {
+      errorMVC::errorSQL("Echec de connection a la base sql");
+    }
+    return ($this->_db);
   }
-
-  public static function getInstance($serveur, $user, $pass, $bd)
+  public function getInstance($serveur, $user, $pass, $bd)
   {
-    
+    if (!isset($this->_db)) {
+      self::connect($serveur, $user, $pass, $bd);
+    }
+    return ($this->_db);
   }
   // \multi ton
 
   public function getLastId() // permet de recuperer la derniere clef primaire insere via mysql_insert_id();
   {
-
+    return (mysql_insert_id($this->_db));
   }
 
-  public function select() //
+  public function select($query) //
   {
-
+    $res = mysql_query($query);
+    if (!$res) {
+      errorMVC::errorSQL("Erreur base sql");
+    }
+    
   }
 
   public function delete() // surcouche pour delete
@@ -39,12 +50,17 @@ class		db
 
   }
   
-  public function insert_update() // surcouche pour insert ou update
+  public function insert() // surcouche pour insert
   {
 
   }
 
-  public function escape($value) {} //mysql_real_escape_string
-  public function escape_html($value) {} // htmlentities
+  public function update() // surcouche pour  update
+  {
+
+  }
+
+  public function escape($value) {mysql_real_escape_string($value);} //mysql_real_escape_string
+  public function escape_html($value) {htmlentities($value);} // htmlentities
 }
 ?>
