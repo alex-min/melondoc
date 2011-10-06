b444f913de084e8842bfc02703d8bf72af529a48
minette.alexandre <minette.alexandre@47a1526c-3bc8-30c1-57df-79ecd9ce54fb>
2011-10-06 11:35:59 +0000

    mise a jour de l editeur

---
+++ trunk/application/controllers/editor.php
@@ -0,0 +1,11 @@
+<?php
+
+class EditorController extends controller
+{
+  public function indexAction()
+  {
+    $this->addCss("editor");
+    $this->addJavascript("editor");
+    $this->template->setView("editor");
+  }
+}
