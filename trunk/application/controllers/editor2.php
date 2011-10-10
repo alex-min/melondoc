bfbfb39d2c0f500781addeeef338c341c83410c2
minette.alexandre <minette.alexandre@47a1526c-3bc8-30c1-57df-79ecd9ce54fb>
2011-10-10 19:07:16 +0000

    editor

---
+++ trunk/application/controllers/editor2.php
@@ -0,0 +1,11 @@
+<?php
+
+class Editor2Controller extends controller
+{
+  public function indexAction()
+  {
+    $this->addCss("editor2");
+    $this->addJavascript("jquery-ui-1.8.16.custom.min");
+   $this->template->setView("editor2");
+  }
+}
