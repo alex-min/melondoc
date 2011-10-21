c056e130787549772006ad041b3c33d35687adde
minette.alexandre <minette.alexandre@47a1526c-3bc8-30c1-57df-79ecd9ce54fb>
2011-10-21 17:24:06 +0000

    editor

--- trunk/public/js/editor2.js
+++ trunk/public/js/editor2.js
@@ -1,16 +1,27 @@
-var txt_area = '<div selected="selected" contenteditable="true" class="ed_area" title="Title"></div>\n';
-var ed_inside_title = '<img class="ed_delete" src="/public/images/ed_delete.png" alt="delete"></img>\n' + txt_area;
+var txt_area = '<div contenteditable="true" class="ed_area" title="Title"></div>\n';
+var ed_switch_dialog = '<div class="ed_switch_dialog">'
++  '<img class="ed_menu_icon draggable" alt="title" src="/public/images/ed_title.png" />'
++   '<img class="ed_menu_icon draggable" alt="bullets" src="/public/images/ed_bullets.png" />'
+'</div>';
+var ed_switch_dialog_begin = '<div class="ed_switch_dialog">';
+var ed_switch_dialog_end = '</div>';
+var ed_switch_dialog = '';
+var ed_inside_title = '<img class="ed_switch" src="/public/images/ed_arrow_down.png" />' +  
+    '<img class="ed_delete" src="/public/images/ed_delete.png" alt="delete" />\n' + txt_area;
 var ed_inside_paragraph = '<img class="ed_delete" src="/public/images/ed_delete.png" alt="delete"></img>\n' + txt_area;
 var ed_droppable = '<div class="droppable"></div>\n';
-var ed_bullets_begin = '<ul>\n';
+var ed_bullets_begin = '<img class="ed_delete ed_bullets" src="/public/images/ed_delete_bullets.png"/><ul>\n';
 var ed_bullets_end = '</ul>\n';
 var latex_assoc = new Array();
 var ed_begin_document_demo = "\n\\documentclass{article}\n\n\\begin{document}\n\\title{This is a titlw}\n\\author{Alexandre MINETTE \\\\\n  \\texttt{\\dddddfdf{email:andyr@comp.leeds.ac.uk}}}\n\\date{Mai 2011}\n\n\\section{Introduction}\nMath XXX                               %%%(class number and section) \n\\hfill vjdioguiiuih ihsio osoh \\\\\n\\hfill oshfh osf hsoh sfoh sofh sfo hs fosfh. \\\\\n\\hfill sdihf i o s h f ohsfoho hsfos fhos fhohfo shohsfofhs. \\\\\n\\hfill jfospfj osfho shsof hsofh soh ososfhso. \\\\\n\\hfill shfo sjhfosf oshfoshf oshshf oshf osh sofjsofh sofhsofh snfosjfoshfs sofhsofh. \\\\\n\n\\paragraph{\nMdr.}\n\n\\newpage\n\nodfjposdjfpsdjfp spjf psjfps jpsjfp jpjf pj p jp pfj\n\n\\end{document}\n\n";
+var doc_x = 0;
+var doc_y = 0;
+var ed_menu_spacer = '<div style="ed_menu_spacer"></div>';
 
-latex_assoc['ed_title'] = new Array('\\title\{', '\}\n');
-latex_assoc['ed_paragraph'] = new Array('\\paragraph\{', '\}\n');
-latex_assoc['ed_bullets'] = new Array('\\begin\{itemize\}\n', '\\end\{itemize\}\n');
-latex_assoc['li'] = new Array('\\item ', '\n');
+latex_assoc['ed_title'] = new Array('<title>', '</title>\n');
+latex_assoc['ed_paragraph'] = new Array('<paragraph>', '</paragraph>\n');
+latex_assoc['ed_bullets'] = new Array('<bullets>\n', '</bullets>\n');
+latex_assoc['li'] = new Array('<item> ', '</item>\n');
 
 function resizeTextarea(t) {
     lines = t.value.split('\n');
@@ -37,6 +48,10 @@ function resizeAllTextArea() {
     });
 }
 
+function html_to_latex(html) {
+    var replace = html.replace(/<br *\/?>/g, "\n");
+    return (replace);
+}
 
 function rec_extract(elem) {
     var txt = '';
@@ -46,19 +61,18 @@ function rec_extract(elem) {
 	    if ($(child[t]).hasClass('ed_title'))
 	    {
 		txt += latex_assoc['ed_title'][0] 
-		    +  $($(child[t]).find('div')[0]).html()
+		    +  html_to_latex($($(child[t]).find('div')[0]).html())
 		    + latex_assoc['ed_title'][1];
 	    }
 	   else if ($(child[t]).hasClass('ed_paragraph'))
 	    {
 		txt += latex_assoc['ed_paragraph'][0] 
-		    +  $($(child[t]).find('div')[0]).html()
+		    +  html_to_latex($($(child[t]).find('div')[0]).html())
 		    + latex_assoc['ed_paragraph'][1];
 	    }
 	   else if ($(child[t]).hasClass('ed_bullets'))
 	    {
 		txt += rec_extract($(child[t]));
-		console.log($(child[t]));
 	    }
 	    else if ($(child[t]).get(0).tagName.toLowerCase() == "ul")
 		txt += latex_assoc['ed_bullets'][0] + rec_extract($(child[t])) + latex_assoc['ed_bullets'][1];
@@ -67,8 +81,6 @@ function rec_extract(elem) {
 		var tmp = rec_extract($(child[t]));
 		if (tmp != '')
 		    txt += latex_assoc['li'][0] + tmp + latex_assoc['li'][1];
-		console.log("miam");
-		console.log($($(child[t])));
 	    }
 	    else
 		rec_extract($(child[t]));
@@ -78,11 +90,12 @@ function rec_extract(elem) {
 }
 	
 function ed_generateLatex() {
-    var txt = '';
+    var txt = '<xml>\n';
     $('.ed_documentarea').each(function () {
 //	console.log($(this));
 	txt += rec_extract($(this));
     });
+    txt += '\n</xml>';
     alert(txt);
 }
 
@@ -92,10 +105,10 @@ function ed_renderToLatex()
 	   {tek : ed_begin_document_demo},
 	   function (data) {
 	       $('.ed_frame').html(data);
-	       console.log(data);
 	       loadPage();
 	   });    
 }
+
 
 $('.ed_viewer').live('click', function () {
     if ($('.ed_popup').css('display') == 'none') {
@@ -107,9 +120,29 @@ $('.ed_viewer').live('click', function () {
     }
 });
 
-$(".ed_documentarea").live('click', function () {    
+$('.ed_area').live('keydown', function () {
+    var cur_area = $(this);
+    $(".ed_area").each(function () {
+	console.log("here !!");
+	if ($(this).attr("_moz_dirty") !== undefined) {
+	    console.log($(this));
+	    $(this).remove();
+//	    cur_area.remove();
+//	console.log("here !!");
+	    cur_area.text(cur_area.text() + 'lol');
+	}
+    });
+       
+});
+
+$('.ed_area').live('click', function () {
 
 });
+
+$(".ed_documentarea").live('click', function (event) {    
+
+});
+
 
 $(".ed_delete").live('click', function () {
     $(this).parent().remove();
@@ -137,12 +170,38 @@ $(".ed_delete").live('click', function () {
     }
     $('li').each(function () {
 	if ($(this).children().length == 0)
-	    $(this).remove();	
+	    $(this).remove();
+    });
+    $('li').each(function () {	
+	// if they are two droppable 
+	if ($(this).next().length == 1 && $(this).next().get(0).tagName.toLowerCase() == "li"
+	   && $(this).length == 1 && $(this).children().hasClass("droppable") && 
+	    $(this).next().children().hasClass("droppable"))
+	    $(this).remove();
     });
 });
 
-$("#ed_textzone").append('<div class="ed_documentarea"></div>');
+var g_to = 0;
+$(".ed_switch").live('click', function () {
+    if (g_to) {
+	g_to = 0;
+	return (false);
+    }
+    $(".ed_switch_dialog").css({
+	visibility:'visible',
+	position:'absolute',
+	top:$(this).position().top + $(this).height() + 5 + 'px',
+	left:$(this).position().left + parseInt($(this).css("margin-left")) + 'px'
+    });
+    $(".ed_switch_dialog").slideToggle(800);    
+ 
+    g_to = 1;
+    return (false);
+});
 
+
+$("#ed_textzone").append('<div ieclass="ed_documentarea" class="ed_documentarea"></div>');
+$('.ed_documentarea').css({display: 'block', width: doc_x + 'px', height: doc_y + 'px'});
 
 $("#ed_menu").live('click', function () {
     // remove draggable propertie
@@ -152,6 +211,7 @@ $("#ed_menu").live('click', function () {
 
 
 function dropEvent(event, ui) {
+    console.log(ui);
     var active_element = $($('.dr_active')[0]);
     switch(ui.draggable.attr('alt')) {
     case 'title':
@@ -172,12 +232,24 @@ function dropEvent(event, ui) {
     });
 }
 
+
 $(document).ready(function () {
     var x = $(document).width() - $(document).width() / 3;
-    var y = $(document).height();
-    $("style").append('.ed_documentarea {display:block'
-		     + ';width:' + x + 'px'
-		     + ';height:' + y + 'px' + '}');
+  //  var y = $(document).height();
+    var y = 5000;
+    if (navigator.appVersion.match(/MSIE/) || 1) { // FIX IE8 BUG
+	$('.ed_documentarea').css({display: 'block', width: x + 'px', height: y + 'px'});
+	doc_x = x;
+	doc_y = y;
+/*	$("style").append('<div inside=".ed_documentarea {display:block'
+			  + ';width:' + x + 'px'
+			  + ';height:' + y + 'px' + '}"></div>');*/
+    }
+    else {
+	$("style").append('.ed_documentarea {display:block'
+			  + ';width:' + x + 'px'
+			  + ';height:' + y + 'px' + '}');
+    }
     $('.draggable').each(function () { $(this).draggable(
 	{
 	    cursor : 'move',
@@ -193,6 +265,18 @@ $(document).ready(function () {
 	out: outEvent
     } );
     resizeAllTextArea();
+    $('.ed_documentarea').css({display: 'block', width: doc_x + 'px', height: doc_y + 'px'});
+    if ($(".ed_switch_dialog").length == 0) {
+	ed_switch_dialog += ed_switch_dialog_begin;
+	$('.ed_switchable').each(function () {
+	    if ($(this).hasClass("ed_switchable")) {
+		ed_switch_dialog += ed_menu_spacer + $(this).clone().wrap('<div>').parent().html();
+	    }
+	});
+	ed_switch_dialog += ed_switch_dialog_end;
+	$("body").html(ed_switch_dialog + $("body").html());
+	$(".ed_switch_dialog").slideToggle(800);
+    }
 });
 
 function droppable_to_title(drop) {
@@ -243,9 +327,8 @@ function droppable_to_paragraph(drop) {
     drop.html(add_before_title + ed_inside_title);
     if (type == 'li')
 	drop = drop.parent();
-	drop.before(add_before_droppabe + ed_droppable + add_after_droppable);
+    drop.before(add_before_droppabe + ed_droppable + add_after_droppable);
     drop.after(add_before_droppabe + ed_droppable + add_after_droppable);
-	
     $('.droppable').droppable( {
 	drop: dropEvent,
 	over: overEvent,
@@ -265,7 +348,6 @@ function droppable_to_bullets(drop) {
 	add_after_droppable = '</li>\n';
 	add_after_title = '</li>\n';
     }
-    console.log('ed_bullets call');
     drop.droppable({disabled: true});
     drop.removeClass('droppable dr_active ui-droppable');
     drop.addClass("ed_block ed_bullets");
@@ -300,7 +382,21 @@ function overEvent(event, ui) {
 function dragCall(event, ui)  {
 }
 
-function draggableHelper () {
+function draggableHelper () {    
+    var img = '<img id="drag_elm" src="/public/images/ed_content.png" width="50" height="50"/>';
+    
+    if ( navigator.appVersion.match(/MSIE 8.0/) || navigator.appVersion.match(/MSIE 7.0/)) {
+	$(document).append(img);
+	console.log('hello world');
+	$(document).mousemove(function (e) {
+	    $('#drag_elm').css({diplay:'block', position:'fixed', top: e.pageY + 'px', left: e.pageX + 'px'});
+	});
+	document.onmouseup = function () {
+	    $('#drag_elm').css({display:'none'});
+//	    document.onmousedown = undefined;
+//	    document.onmouseup = undefined;
+	}
+    }
     return ('<img id="drag_elm" src="./public/images/ed_content.png" width="50" height="50"/>');
 }
 
