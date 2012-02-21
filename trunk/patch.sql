5cadc13c0e1b06d3928d66fe23263c5e3105dfbe
julien.singler <julien.singler@47a1526c-3bc8-30c1-57df-79ecd9ce54fb>
2012-02-21 10:18:37 +0000

    yeah ajax sans bizarrerie

--- trunk/patch.sql
+++ trunk/patch.sql
@@ -1,7 +1,9 @@
 ALTER TABLE `documents` ADD `user_id` INT NOT NULL AFTER `id_document`;
-ALTER TABLE `documents` ADD `nom` VARCHAR( 255 ) NOT NULL ;
+ALTER TABLE `documents` ADD `nom` VARCHAR( 255 ) NOT NULL;
 ALTER TABLE `documents` ADD `deleted` TINYINT NOT NULL;
-ALTER TABLE `users` ADD `deleted` TINYINT NOT NULL ;
+ALTER TABLE `users` ADD `deleted` TINYINT NOT NULL;
 ALTER TABLE `templates` ADD `template_image` VARCHAR( 255 ) NOT NULL;
 ALTER TABLE `documents` DROP `path_template`;
 ALTER TABLE `documents` ADD `template_id` INT NOT NULL;
+ALTER TABLE `categories` ADD `avatar` VARCHAR( 255 ) NOT NULL;
+ALTER TABLE `templates` ADD `categorie_id` INT NOT NULL;
