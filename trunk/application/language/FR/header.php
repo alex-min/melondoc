cfc1491c45d515dced21c2006d4bdb9bf9c2a05a
julien.singler <julien.singler@47a1526c-3bc8-30c1-57df-79ecd9ce54fb>
2012-02-21 16:47:03 +0000

    header language

---
+++ trunk/application/language/FR/header.php
@@ -0,0 +1,28 @@
+<?php
+$_['header_deconnexion'] = "Déconnexion";
+$_['header_connexion'] = "Connexion";
+$_['header_index'] = "Accueil";
+$_['header_account'] = "Mes infos";
+$_['header_connexion_inscription'] = "Connexion / Inscription";
+$_['title'] = "MelonDoc";
+$_['header_email'] = "Email";
+$_['header_firstname'] = "Prénom";
+$_['header_lastname'] = "Nom";
+$_['header_password'] = "Mot de passe";
+$_['header_verif_password'] = "Vérification du mot de passe";
+$_['header_forum'] = "Forum";
+$_['header_files'] = "Mes Fichiers";
+$_['header_inscription'] = "Inscription";
+$_['header_login'] = "Login";
+$_['header_need_login'] = "Vous devez être connecté pour accéder à cette page";
+$_['header_facebook'] = "Facebook";
+$_['email_invalid'] = "Email incorrect !";
+$_['firstname_invalid'] = "Prénom incorrect!";
+$_['lastname_invalid'] = "Nom incorrect !";
+$_['password_invalid'] = "Mot de passe incorrect";
+$_['login_invalid'] = "Le login doit être composé d'au moins 4 caractères alphanumériques. Les tirets et underscores sont tolérés.";
+$_['header_forum'] = "Forum";
+$_['header_contact'] = "Contact";
+$_['header_files'] = "Mes documents";
+$_['header_create_new'] = "Créer un nouveau document";
+?>
