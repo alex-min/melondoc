d8820e6ee378389ece95caa1c00e4b5b27c44d4c
minette.alexandre@gmail.com <minette.alexandre@gmail.com@47a1526c-3bc8-30c1-57df-79ecd9ce54fb>
2012-04-17 17:46:32 +0000

    d

--- trunk/application/controllers/savedoc.php
+++ trunk/application/controllers/savedoc.php
@@ -3,7 +3,7 @@ class SavedocController extends controller {
  public function		indexAction()
   {
   	if (isset($_POST['doc'])) {
-  		$this->model->save($_POST['doc'], $_POST['id']);
+  		$this->model->save($_POST['doc'], $_POST['id'],$_POST['name']);
   	}
   }
 }
